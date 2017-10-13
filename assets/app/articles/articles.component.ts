ae5f82f6e7ba391eb1fb84c05eee3c375d80ad51
treywilson3 <trey.wilson3@gmail.com>
2017-10-13 12:33:59 -0400

    Added nicer design to favorite button

--- assets/app/articles/articles.component.ts
+++ assets/app/articles/articles.component.ts
@@ -10,6 +10,10 @@ import {Router} from "@angular/router";
 })
 export class ArticlesComponent implements OnInit {
   articles: Article[];
+  // if favorite = false then full heart is not shown. if true then heart shown
+  favorite: boolean = false;
+  // clicked will be used to determine if we should keep hovering effect on
+  clicked: boolean = false;
 
   constructor(private router: Router, private articleService: ArticlesService) { }
 
@@ -20,6 +24,29 @@ export class ArticlesComponent implements OnInit {
               this.articles = articles;
             }
         );
+  }
+
+  addFavorite(){
+    // toggle full and empty heart
+    this.clicked = !this.clicked;
+    if (this.clicked === true){
+      // if clicked then add to database and show full heart
+      this.favorite = true;
+    } else { // if false then remove from database and show empty heart
+      this.favorite = false;
+    }
+  }
+  hoverFavorite(){
+    // if clicked is false then show hover effect, else dont
+    if (this.clicked === false){
+      this.favorite = true;
+    }
+  }
+  removeHoverFavorite(){
+    // if clicked is false then show hover effect, else dont
+    if (this.clicked === false){
+      this.favorite = this.favorite = false;
+    }
   }
 
 }
