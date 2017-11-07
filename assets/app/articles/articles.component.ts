ac389d7f0bce2bde585a3469aa86b06482379479
treywilson3 <trey.wilson3@gmail.com>
2017-11-06 20:29:23 -0500

    Adding before wiping computer

--- assets/app/articles/articles.component.ts
+++ assets/app/articles/articles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ArticlesService} from "./articles.service";
 import {Article} from "./article.model";
 import {Router} from "@angular/router";
@@ -14,7 +14,8 @@ export class ArticlesComponent implements OnInit {
   articles: Article[];
   favorites: Favorite[];
 
-  constructor(private router: Router, private articleService: ArticlesService, private favoriteService: FavoriteService) { }
+  constructor(private router: Router, private articleService: ArticlesService, private favoriteService: FavoriteService) {
+  }
 
   ngOnInit() {
     this.articleService.getArticles()
@@ -23,19 +24,19 @@ export class ArticlesComponent implements OnInit {
               this.articles = articles;
             }
         );
-    // this.favoriteService.getFavorites()
-    //     .subscribe(
-    //         (favorites: Favorite[]) => {
-    //           this.favorites = favorites;
-    //         }
-    //     );
+    this.favoriteService.getFavorites()
+        .subscribe(
+            (favorites: Favorite[]) => {
+              this.favorites = favorites;
+            }
+        );
   }
 
-  addFavorite(articleId: string, article: any){
+  addFavorite(articleId: string, article: any) {
     // toggle full and empty heart
     article.clicked = !article.clicked;
     let userId = localStorage.getItem('userId');
-    if (article.clicked === true){
+    if (article.clicked === true) {
       // if clicked then add to database and show full heart
       let favorite = new Favorite(articleId, userId);
       this.favoriteService.addFavorite(favorite)
@@ -50,11 +51,12 @@ export class ArticlesComponent implements OnInit {
       //     );
     }
   }
+
   favoriteBackground(article: any) {
     // if clicked then disable hovering (which is done by this first if)
-    if (article.clicked){
+    if (article.clicked) {
       return 'glyphicon-heart'
-    } else if (article.iconHover == true){ // if the user hovers, then show full heart
+    } else if (article.iconHover == true) { // if the user hovers, then show full heart
       return 'glyphicon-heart';
     } else if (!article.clicked) {
       // since article.clicked is not instantiated at first then set this as the else so empty heart appears no matter what
